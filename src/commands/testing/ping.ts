164344b391e1051a4edd6c4d64c8db134e1119cd
phxgg <stamatis.1702@gmail.com>
2026-01-27 20:44:45 +0200

    chore: update ping command

--- src/commands/testing/ping.ts
+++ src/commands/testing/ping.ts
@@ -1,29 +1,10 @@
-import {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  AutocompleteInteraction,
-  ChatInputCommandInteraction,
-  SlashCommandBuilder,
-} from 'discord.js';
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { IBaseCommand } from '@/commands/IBaseCommand';
-
-import inSameVoiceChannel from '@/middleware/inSameVoiceChannel';
 
 export default class PingCommand implements IBaseCommand {
   data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with pong!');
-
-  /**
-   * Example middleware array for this command.
-   */
-  middleware = [inSameVoiceChannel];
-
-  /**
-   * Example autocomplete method for this command.
-   */
-  // async autocomplete(interaction: AutocompleteInteraction) {
-  //   // autocomplete logic here
-  // }
 
   /**
    * This method is called when the command is executed.
